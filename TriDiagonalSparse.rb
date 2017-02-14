6b81676a072cb84138538a440ba448371a63fd5f
aaronphilips <aphilips@ualberta.ca>
2017-02-14 09:34:28 -0700

    tridiagonal stuff

--- TriDiagonalSparse.rb
+++ TriDiagonalSparse.rb
@@ -1,8 +1,8 @@
-require 'SparseMatrix'
+require_relative 'SparseMatrix'
 class TriDiagonalSparse<SparseMatrix
 
-	def initialize(args)
-		super(args)
+	def initialize(*args)
+		super(*args)
 		invariants
 	end
 
@@ -10,14 +10,26 @@ class TriDiagonalSparse<SparseMatrix
 		assert_equal @dimension.length, 2, "Not a 2D TriDiagonalSparse Matrix"
 		assert(@dimension[0]>=6, "Not a valid TriDiagonalSparse Matrix. Too small")
 		assert(@dimension[1]>=6, "Not a valid TriDiagonalSparse Matrix. Too small")
-		super.invariants()
+
+		super
 
 	end
 
-	# better determinant functions
-	def determinant
+	def check_tri_diagonal
+		retval=true
+		self.get_sparse_matrix_hash.each do |key,value|
+			if(key[0]>key[1]+1||key[0]<key[1]-1)
+				return false
+			end
+		end
+		return retval
 	end
-
-
 end
-b=TriDiagonalSparse.new([3,2])
+b=TriDiagonalSparse.new(6,6)
+b.insert_at([0,0],23)
+b.insert_at([1,0],23)
+b.insert_at([0,1],23)
+b.insert_at([2,1],23)
+b.insert_at([1,2],23)
+b.insert_at([3,1],23123)
+puts b.check_tri_diagonal
